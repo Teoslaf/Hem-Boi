4366e5fc71e446d7fb42a237a8c9e0e6468cb69a
Teo <teotaneski2@gmail.com>
2020-02-18 13:31:40 +0100

    CSS

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send('Mjau');
+	res.sendFile('index.html', { root: '.' });
 });
 
 app.listen(3000, () => {
